cd341cb67dbe9fa034a4d5aaafb108bda35ee51f
Thomas Harr <xDevThomas@gmail.com>
2023-01-05 14:33:28 +0100

    fix: Parse wf.args to load update for newer version
    - The 'magic' arguments are only evaluated when the workflow args are parsed via. wf.args
    - Encapsulate the workflow functionality in the BatteryInfo class

--- src/battery.py
+++ src/battery.py
@@ -41,237 +41,241 @@ ICON_HEALTH = 'icons/health.png'
 ICON_SERIAL = 'icons/serial.png'
 
 
-def battery_info_program():
-    """Return path to BateryInfo executable.
+class BatteryInfo(object):
+    """Encapsulates the functionallity of the battery workflow
 
-    Returns:
-        str: Path to ``BatteryInfo`` executable.
+    Display battery information of your Apple laptop.
     """
-    return wf.cachefile('BatteryInfo')
+
+    def __init__(self) -> None:
+        """Create a new ``BatteryInfo`` object.
+        """
+        self.wf = None
+
+    def battery_info_program(self):
+        """Return path to BateryInfo executable.
+
+        Returns:
+            str: Path to ``BatteryInfo`` executable.
+        """
+        return self.wf.cachefile('BatteryInfo')
 
 
-def battery_info_check_update():
+    def battery_info_check_update(self):
 
-    key = '__battery_info_update'
-    # Cache that a workflow update is available. This will lead to a BatteryInfo update
-    if wf.update_available:
-        status = wf.cache_data(key, {
-            'available': True,
-            'version' : wf.version
-        })
-        return False
+        key = '__battery_info_update'
+        # Cache that a workflow update is available. This will lead to a BatteryInfo update
+        if self.wf.update_available:
+            status = self.wf.cache_data(key, {
+                'available': True,
+                'version' : self.wf.version
+            })
+            return False
 
-    status = wf.cached_data(key, max_age=0)
-    if not status or not status.get('available'):
-        return False
+        status = self.wf.cached_data(key, max_age=0)
+        if not status or not status.get('available'):
+            return False
 
-    return True
+        return True
 
 
-def install_battery_info():
-    bi = battery_info_program()
-    if os.path.exists(bi):
-        os.unlink(bi)
+    def install_battery_info(self):
+        bi = self.battery_info_program()
+        if os.path.exists(bi):
+            os.unlink(bi)
 
-    tempdir = tempfile.mkdtemp(prefix='aw-', dir=wf.cachedir)
-
-    try:
-        os.chdir(tempdir)
-        source_code = os.path.join(wf.workflowdir, 'BatteryInfo.swift')
-        binary_src = os.path.join(tempdir, 'BatteryInfo')
-        binary_dest = os.path.join(wf.cachedir, 'BatteryInfo')
-
-        cmd = [
-            'swiftc',
-            source_code]
-
-        retcode = subprocess.call(cmd)
-        if retcode != 0:
-            raise RuntimeError(f'BatteryInfo compilation exited with {retcode}')
-
-        if not os.path.exists(binary_src):  # pragma: nocover
-            raise ValueError(f'Compiled BatteryInfo file not found: {binary_src}')
-
-        shutil.move(binary_src, binary_dest)
-
-        if not os.path.exists(binary_dest):  # pragma: nocover
-            raise ValueError(f'Compiled BatteryInfo file not found: {binary_src}')
-
-    finally:
-        os.chdir(wf.workflowdir)
+        tempdir = tempfile.mkdtemp(prefix='aw-', dir=self.wf.cachedir)
 
         try:
-            shutil.rmtree(tempdir)
-        except OSError:  # pragma: no cover
-            pass
+            os.chdir(tempdir)
+            source_code = os.path.join(self.wf.workflowdir, 'BatteryInfo.swift')
+            binary_src = os.path.join(tempdir, 'BatteryInfo')
+            binary_dest = os.path.join(self.wf.cachedir, 'BatteryInfo')
 
-    # Update successfully
-    wf.cache_data('__battery_info_update', {
-            'available': False,
-            'version' : wf.version
-        })
+            cmd = [
+                'swiftc',
+                source_code]
 
+            retcode = subprocess.call(cmd)
+            if retcode != 0:
+                raise RuntimeError(f'BatteryInfo compilation exited with {retcode}')
 
-def convert_battery_info(info):
-  try:
-    return True, json.loads(info)
-  except ValueError as e:
-    return False, None
+            if not os.path.exists(binary_src):  # pragma: nocover
+                raise ValueError(f'Compiled BatteryInfo file not found: {binary_src}')
 
+            shutil.move(binary_src, binary_dest)
 
-def get_battery_info() -> tuple[bool, dict | str]:
-    """Get the system battery inforamtion from your laptop
+            if not os.path.exists(binary_dest):  # pragma: nocover
+                raise ValueError(f'Compiled BatteryInfo file not found: {binary_src}')
 
-    Returns:
-        tuple[bool, dict | str]: In case of success True and the battery info
-            otherwiese False and None
-    """
-    bi = battery_info_program()
+        finally:
+            os.chdir(self.wf.workflowdir)
 
-    # Install if BatteryInfo does not exist
-    if (not os.path.exists(bi)) or battery_info_check_update():
-        install_battery_info()
+            try:
+                shutil.rmtree(tempdir)
+            except OSError:  # pragma: no cover
+                pass
 
-
-    output = run_command([bi]).decode('utf-8')
-    return convert_battery_info(output)
-
-
-def show_error(battery_err):
-    wf.add_item(title='Retrieved an error',
-            subtitle=battery_err,
-            valid=False,
-            icon=ICON_ERROR
-        )
+        # Update successfully
+        self.wf.cache_data('__battery_info_update', {
+                'available': False,
+                'version' : self.wf.version
+            })
 
 
-def show_info(battery_info):
-    raw_current_capacity = battery_info['AppleRawCurrentCapacity']
-    raw_max_capacity = battery_info['AppleRawMaxCapacity']
-    design_capacity = battery_info['DesignCapacity']
-    charge = round(raw_current_capacity * 100 / raw_max_capacity, 2)
-    loaded = '●' * round(charge/10)
-    unloaded = '○' * round(10-charge/10)
-    cells = f'{loaded}{unloaded}'
-    status_info = 'Draining...'
-    charging = battery_info['IsCharging']
-    time_to_empty = battery_info['AvgTimeToEmpty']
-    time_to_full = battery_info['AvgTimeToFull']
-    time_left = 'Calculating…'
-    time_full = 'Calculating…'
-    fully_charged = battery_info['FullyCharged']
-    external = battery_info['ExternalConnected']
+    def convert_battery_info(self, info):
+        try:
+            return True, json.loads(info)
+        except ValueError as e:
+            return False, None
 
-    if time_to_empty < 15000:
-        time_left = f'{round(time_to_empty/60)}:{round(time_to_empty%60)}'
 
-    if time_to_full < 15000:
-        time_full = f'{round(time_to_full/60)}:{round(time_to_full%60)}'
+    def get_battery_info(self) -> tuple[bool, dict | str]:
+        """Get the system battery inforamtion from your laptop
 
-    if charging:
-        time_info = f'{time_full} until full'
-        status_info = 'Charging...'
-        battery_icon = ICON_CHARGING
-    else:
-        if fully_charged:
-            if external:
-                time_info = 'On AC power'
-                status_info = 'Fully Charged'
-                battery_icon = ICON_POWER
-                charge = '100'
-                cells = '●●●●●●●●●●'
+        Returns:
+            tuple[bool, dict | str]: In case of success True and the battery info
+                otherwiese False and None
+        """
+        bi = self.battery_info_program()
+
+        # Install if BatteryInfo does not exist
+        if (not os.path.exists(bi)) or self.battery_info_check_update():
+            self.install_battery_info()
+
+
+        output = run_command([bi]).decode('utf-8')
+        return self.convert_battery_info(output)
+
+
+    def show_error(self, battery_err):
+        self.wf.add_item(title='Retrieved an error',
+                subtitle=battery_err,
+                valid=False,
+                icon=ICON_ERROR
+            )
+
+
+    def show_info(self, battery_info):
+        raw_current_capacity = battery_info['AppleRawCurrentCapacity']
+        raw_max_capacity = battery_info['AppleRawMaxCapacity']
+        design_capacity = battery_info['DesignCapacity']
+        charge = round(raw_current_capacity * 100 / raw_max_capacity, 2)
+        loaded = '●' * round(charge/10)
+        unloaded = '○' * round(10-charge/10)
+        cells = f'{loaded}{unloaded}'
+        status_info = 'Draining...'
+        charging = battery_info['IsCharging']
+        time_to_empty = battery_info['AvgTimeToEmpty']
+        time_to_full = battery_info['AvgTimeToFull']
+        time_left = 'Calculating…'
+        time_full = 'Calculating…'
+        fully_charged = battery_info['FullyCharged']
+        external = battery_info['ExternalConnected']
+
+        if time_to_empty < 15000:
+            time_left = f'{round(time_to_empty/60)}:{round(time_to_empty%60)}'
+
+        if time_to_full < 15000:
+            time_full = f'{round(time_to_full/60)}:{round(time_to_full%60)}'
+
+        if charging:
+            time_info = f'{time_full} until full'
+            status_info = 'Charging...'
+            battery_icon = ICON_CHARGING
+        else:
+            if fully_charged:
+                if external:
+                    time_info = 'On AC power'
+                    status_info = 'Fully Charged'
+                    battery_icon = ICON_POWER
+                    charge = '100'
+                    cells = '●●●●●●●●●●'
+                else:
+                    time_info = time_left
+                    battery_icon = ICON_FULL
             else:
                 time_info = time_left
-                battery_icon = ICON_FULL
-        else:
-            time_info = time_left
-            battery_icon = ICON_CRITICAL
-            if charge > 80:
-                battery_icon = ICON_FULL
-            elif charge > 50:
-                battery_icon = ICON_MEDIUM
-            elif charge > 10:
-                battery_icon = ICON_LOW
+                battery_icon = ICON_CRITICAL
+                if charge > 80:
+                    battery_icon = ICON_FULL
+                elif charge > 50:
+                    battery_icon = ICON_MEDIUM
+                elif charge > 10:
+                    battery_icon = ICON_LOW
 
-    temperature = round(battery_info['Temperature'] / 10 - 273, 2)
-    cycle_count = battery_info['CycleCount']
-    health = round(raw_max_capacity * 100 / design_capacity)
-    serial = battery_info['Serial']
+        temperature = round(battery_info['Temperature'] / 10 - 273, 2)
+        cycle_count = battery_info['CycleCount']
+        health = round(raw_max_capacity * 100 / design_capacity)
+        serial = battery_info['Serial']
 
-    wf.add_item(title=f'{charge}% {cells}',
-            subtitle=status_info,
+        self.wf.add_item(title=f'{charge}% {cells}',
+                subtitle=status_info,
+                valid=False,
+                icon=battery_icon
+            )
+        self.wf.add_item(title=time_info,
+            subtitle='Time Left',
             valid=False,
-            icon=battery_icon
+            icon=ICON_CLOCK
         )
-    wf.add_item(title=time_info,
-        subtitle='Time Left',
-        valid=False,
-        icon=ICON_CLOCK
-    )
-    wf.add_item(title=f'{temperature} °C',
-        subtitle='Temperature',
-        valid=False,
-        icon=ICON_TEMP
-    )
-    wf.add_item(title=cycle_count,
-        subtitle='Charge Cycles Completed',
-        valid=False,
-        icon=ICON_CYCLES
-    )
-    wf.add_item(title=f'{health}%',
-        subtitle='Health',
-        valid=False,
-        icon=ICON_HEALTH
-    )
-    wf.add_item(title=serial,
-        subtitle='Serial',
-        valid=False,
-        icon=ICON_SERIAL
-    )
+        self.wf.add_item(title=f'{temperature} °C',
+            subtitle='Temperature',
+            valid=False,
+            icon=ICON_TEMP
+        )
+        self.wf.add_item(title=cycle_count,
+            subtitle='Charge Cycles Completed',
+            valid=False,
+            icon=ICON_CYCLES
+        )
+        self.wf.add_item(title=f'{health}%',
+            subtitle='Health',
+            valid=False,
+            icon=ICON_HEALTH
+        )
+        self.wf.add_item(title=serial,
+            subtitle='Serial',
+            valid=False,
+            icon=ICON_SERIAL
+        )
 
 
-def run():
-    """Get the system battery inforamtion from your laptop
+    def run(self, wf):
+        """Run the the main workflow logic
 
-    Returns:
-        int: The Exit status.
+        Args:
+            wf (Workflow): The Alfred-PyWorkflow instance
 
-    """
-    valid, battery_info = get_battery_info()
-
-    if not valid:
-        show_error(battery_info)
-    else:
-        show_info(battery_info)
-
-    wf.send_feedback()
-    return 0
-
-
-def main(wf):
-    """Run the the main workflow logic
-
-    Args:
-        wf (Workflow): The Alfred-PyWorkflow instance
-
-    """
-
-    # Notify user if update is available
-    # ------------------------------------------------------------------
-    if wf.update_available:
-        battery_info_check_update()
-        wf.add_item('Workflow Update is Available',
-                    '↩ or ⇥ to install',
-                    autocomplete='workflow:update',
-                    valid=False,
-                    icon=ICON_UPDATE)
+        """
+        self.wf = wf
+        # Needed as otherwise the magic arguments dont work e.g. the workflow:update
+        args = self.wf.args
+        # Notify user if update is available
+        # ------------------------------------------------------------------
+        if wf.update_available:
+            self.battery_info_check_update()
+            self.wf.add_item('Workflow Update is Available',
+                        '↩ or ⇥ to install',
+                        autocomplete='workflow:update',
+                        valid=False,
+                        icon=ICON_UPDATE)
 
 
-    return run()
+        valid, battery_info = self.get_battery_info()
+
+        if not valid:
+            self.show_error(battery_info)
+        else:
+            self.show_info(battery_info)
+
+        self.wf.send_feedback()
+        return 0
 
 
 if __name__ == '__main__':
     wf = Workflow(update_settings=UPDATE_SETTINGS,
                   help_url=HELP_URL)
     log = wf.logger
-    sys.exit(wf.run(main))
+    batt = BatteryInfo()
+    sys.exit(wf.run(batt.run))
